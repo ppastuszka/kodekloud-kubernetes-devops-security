7fc0937589411ae2cd9f42f02ae3af4cb9f60fb4
ppastuszka <pawel.pastuszka@iteo.com>
2024-06-17 12:48:42 +0200

    Refactor Docker image vulnerability scan in trivy-docker-image-scan.sh

---
+++ trivy-docker-image-scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dockerImageName $(awk 'NR==1 {print $2}' Dockerfile)
+echo $dockerImageName
+
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --error-code 0 --severity HIGH $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --error-code 1 --severity CRITICAL $dockerImageName
+
+exit_code $?
+echo "Exit Code: $exit_code"
+
+if [ $exit_code -eq 0 ]; then
+    echo "Trivy scan passed successfully"
+else
+    echo "Trivy scan failed"
+    exit 1
+fi
