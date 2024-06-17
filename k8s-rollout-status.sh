ac3ff46d8506f4a3e21151fe19105f5e5956d4bd
ppastuszka <pawel.pastuszka@iteo.com>
2024-06-17 14:39:23 +0200

    Refactor k8s-rollout-status.sh to fix syntax error in if statement

--- k8s-rollout-status.sh
+++ k8s-rollout-status.sh
@@ -2,7 +2,7 @@
 
 sleep 60s
 
-if [[ $(kubectl -n default rollout status deploy $(deploymentName --timeout 5s) != *"successfully rolled out"* ]]) ]];
+if [[ $(kubectl -n default rollout status deploy $(deploymentName --timeout 5s) != *"successfully rolled out"* ]];
 then
   echo "Rollout failed"
   kubectl -n default rollout undo deploy $(deploymentName)
