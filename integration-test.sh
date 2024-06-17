f52ea3ca60cd9c09cc659925ae574eb591544a3c
ppastuszka <pawel.pastuszka@iteo.com>
2024-06-17 17:22:25 +0200

    Refactor integration-test.sh to fix condition for response check

--- integration-test.sh
+++ integration-test.sh
@@ -10,7 +10,7 @@ if [[ ! -z "$PORT" ]]; then
   response=$(curl -s $applicationURL:$PORT/$applicationURI)
   http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT/$applicationURI)
 
-  if [[ "$response" == 100 ]]; then
+  if [[ "$response" == 100* ]]; then
     echo "Increment test passed"
     exit 0
   else
